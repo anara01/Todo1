1195935ed7b7125efe4567a12fde9e95926ae9c4
Anara01 <anaraerkebaeva2@gmail.com>
2021-11-07 13:31:48 +0300

    commit

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import ToDo
+
+from .models import ToMeet
+
+admin.site.register(ToDo)
+
+admin.site.register(ToMeet)
--- main/models.py
+++ main/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+
+class ToDo(models.Model):
+    text = models.CharField(max_length=100)
+    created_at = models.DateField(auto_now_add=True)
+    is_closed = models.BooleanField(default=False)
+    is_favorite = models.BooleanField(default=False)
+
+
+class ToMeet(models.Model):
+    persone = models.CharField(max_length=30)
+    phone_number = models.BigIntegerField()
+    date_of_meeting = models.DateField()
+    is_closed = models.BooleanField(default=False)
+    is_favorite = models.BooleanField(default=False)
