ae57d1c9ac5f605437104a51e446ef7213a52ddb
Anara01 <anaraerkebaeva2@gmail.com>
2021-11-07 13:55:29 +0300

    commit

--- main/views.py
+++ main/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from .models import ToDo
 # Create your views here.
 
 
@@ -8,4 +8,5 @@ def homepage(request):
 
 
 def test(request):
-    return render(request, "test.html")
+    todo_list = ToDo.objects.all()
+    return render(request, "test.html", {"todo_list": todo_list})
